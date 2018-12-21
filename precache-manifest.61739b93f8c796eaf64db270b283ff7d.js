d73c368dd3abdfbe9599565d81eebe87b80513f0
niklasbyggmastar <niklas.byggmastar@gmail.com>
2018-12-21 18:07:38 +0200

    Updates

---
+++ precache-manifest.61739b93f8c796eaf64db270b283ff7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79efe98ad17809222261",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/runtime~main.79efe98a.js"
+  },
+  {
+    "revision": "8f48ee5f10ff437e4d4f",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/main.8f48ee5f.chunk.js"
+  },
+  {
+    "revision": "50be0ef6409c52304a14",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/1.50be0ef6.chunk.js"
+  },
+  {
+    "revision": "8f48ee5f10ff437e4d4f",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/css/main.ebf4792a.chunk.css"
+  },
+  {
+    "revision": "50be0ef6409c52304a14",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/css/1.2bbbfd63.chunk.css"
+  },
+  {
+    "revision": "622841ee17220b9e4903e7414ceb10fa",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/index.html"
+  }
+];
