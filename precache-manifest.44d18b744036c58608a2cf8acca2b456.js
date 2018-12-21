ac9a310510bec3a1386809c08d99d4f759e8ed13
niklasbyggmastar <niklas.byggmastar@gmail.com>
2018-12-21 18:21:33 +0200

    Updates

--- precache-manifest.44d18b744036c58608a2cf8acca2b456.js
+++ precache-manifest.44d18b744036c58608a2cf8acca2b456.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/runtime~main.79efe98a.js"
   },
   {
-    "revision": "8f48ee5f10ff437e4d4f",
-    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/main.8f48ee5f.chunk.js"
+    "revision": "c17ed795666729b8c1d3",
+    "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/main.c17ed795.chunk.js"
   },
   {
     "revision": "50be0ef6409c52304a14",
     "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/js/1.50be0ef6.chunk.js"
   },
   {
-    "revision": "8f48ee5f10ff437e4d4f",
+    "revision": "c17ed795666729b8c1d3",
     "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/css/main.ebf4792a.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-niklasbyggmastar/static/css/1.2bbbfd63.chunk.css"
   },
   {
-    "revision": "622841ee17220b9e4903e7414ceb10fa",
+    "revision": "95c9e2cbb17845ff06a39261ef56c420",
     "url": "/2018-interactive-web-apps-project-niklasbyggmastar/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2018-interactive-web-apps-project-niklasbyggmastar/precache-manifest.61739b93f8c796eaf64db270b283ff7d.js"
+  "/2018-interactive-web-apps-project-niklasbyggmastar/precache-manifest.44d18b744036c58608a2cf8acca2b456.js"
 );
 
 workbox.clientsClaim();
