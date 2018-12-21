0c932430c9819171b03a9ee5548b40f60cef0530
niklasbyggmastar <niklas.byggmastar@gmail.com>
2018-12-21 17:46:49 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import * as serviceWorker from './serviceWorker';
 
 import App from './components/App';
 
 // Entry point, render the App component in the root div that is in the index.html
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
